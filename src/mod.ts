5377599204afb8418f720599705c47af990be677
mBartoldus <mbartoldus@gmail.com>
2024-06-25 16:56:01 -0400

    now exports makeWorker

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,3 @@
 export type { PropertyMetadata, ObjectMetadata } from "./types.ts"
 export { declare, allocateData, prepareObject, get, set, assign, share, accept, manifestObject } from "./threadables.ts"
+export { makeWorker } from './makeWorker.ts'
